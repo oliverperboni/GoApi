0ae8875f69c6927f05337f4f62fae0fca22ebe64
Oliver Perboni <oliverperboni@pop-os.home>
2024-08-05 14:31:10 +0100

    begin of the user implementation

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"github.com/oliverperboni/GoTomekeeper/schemas"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func CreateUserRepository(db *gorm.DB) UserRepository {
+	return UserRepository{DB: db}
+}
+
+func (u *UserRepository) CreateUser(user schemas.User) error {
+	return u.DB.Save(user).Error
+}
+
+func (u *UserRepository) GetUser(username string, password string) error {
+	return u.DB.Find("username = ? AND passwordhash = ?", username, password).Error
+}
--- services/userService.go
+++ services/userService.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"github.com/oliverperboni/GoTomekeeper/repository"
+	"github.com/oliverperboni/GoTomekeeper/schemas"
+)
+
+type UserService struct {
+	repo repository.UserRepository
+}
+
+func CreateUserService(r repository.UserRepository) UserService {
+	return UserService{repo: r}
+}
+
+func (u *UserService) CreateUser(user schemas.User) error {
+	return u.repo.CreateUser(user)
+}
+
+func (u *UserService) GetUser(username string, password string) error {
+	return u.repo.GetUser(username, password)
+}
